ef71497e87e1fc0d23f0f4d026e6ef894b054f66
Torkil Djurhuus <torkildjurhuus@gmail.com>
2019-01-28 14:07:37 +0000

    Updates

---
+++ precache-manifest.4ad8ca9410ebadb0eda09dda387ffbd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "5f6f62e42c4a4591ab2d",
+    "url": "/react-tutorial/static/js/main.5f6f62e4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "5f6f62e42c4a4591ab2d",
+    "url": "/react-tutorial/static/css/main.78cdb68a.chunk.css"
+  },
+  {
+    "revision": "0d4e329e81fcf615e477f824c794c209",
+    "url": "/react-tutorial/index.html"
+  }
+];
